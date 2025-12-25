01abd596a554dc4ed4bcae2f335d04db7401cc9e
Sam Evans <evanssw02@outlook.com>
2025-12-25 02:34:53 -0700

    There are now 5 metrics for a good password and each are 20% on the bar

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ let power = document.getElementById("power-point");
 password.oninput = function() {
     let point = 0;
     let value = password.value;
-    let widthPower = ["1%", "25%", "50%", "75%", "100%"];
-    let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3BA62F"]
+    let widthPower = ["1%", "20%", "40%", "60%", "80%", "100%"];
+    let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#f2e74fff", "#BDE952", "#0f9400ff"]
 
-    if (value.length >= 6) {
-        let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
-        arrayTest.forEach((item) => {
-            if (item.test(value)) {
-                point += 1;
-            }
-        });
+    if (value.length >= 8) {
+        point += 1;
     }
+    let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+    arrayTest.forEach((item) => {
+        if (item.test(value)) {
+            point += 1;
+        }
+    });
     power.style.width = widthPower[point];
     power.style.backgroundColor = colorPower[point];
 };
